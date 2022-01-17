53ddaa998bd8a6be6920a5fbd31abfe23bb2f0d5
Finley Edwards <EDW17006550@btc.ac.uk>
2022-01-17 22:46:04 +0000

    Removed predefined functions.

--- Stacks 2/Program.cs
+++ Stacks 2/Program.cs
@@ -7,25 +7,25 @@ namespace Pop_Top_Peek_2
     {
         static void Main(string[] args)
         {
-            List<string> stringList = new List<string>(); //Creates a list that will be acting as a stack.
+            string[] stringArray = new string[0]; //Creates an array that will be acting as a stack.
 
-            string insertString = "Hello"; //Creates a string that will then be added to the list.
-            push(stringList, insertString); //Initiates the push method.
+            string insertString = "Hello"; //Creates a string that will then be added to the array.
+            stringArray = push(stringArray, insertString); //Initiates the push method and sets stringArray to the array that gets returned.
             insertString = "This value is in the middle.";
-            push(stringList, insertString);
+            stringArray = push(stringArray, insertString);
             insertString = "This value is on top.";
-            push(stringList, insertString);
+            stringArray = push(stringArray, insertString);
 
-            peek(stringList); //Initiates the peek method.
+            stringArray = pop(stringArray); //Initiates the pop method and sets stringArray to the array that gets returned.
 
-            pop(stringList); //Initiates the pop method.
+            peek(stringArray); //Initiates the peek method.
 
 
 
 
 
             Console.WriteLine(); //Creates a space in the console to seperate what outputs next.
-            foreach (var item in stringList) //Outputs the remaining items in the list.
+            foreach (var item in stringArray) //Outputs the remaining items in the array.
             {
                 Console.WriteLine(item);
             }
@@ -33,20 +33,33 @@ namespace Pop_Top_Peek_2
 
 
 
-        static void push(List<string> popList, string insertString) //Defines the push method.
+        static string[] push(string[] pushArray, string insertString) //Defines the push method.
         {
-            popList.Insert(0, insertString); //Adds the string into the beginning of the list.
+            string[] tempArray = new string[pushArray.Length + 1]; //Creates a new temporary array with a length one higher than pushArray.
+
+            tempArray[0] = insertString; //sets the first value to insertString.
+            for (int i = 0; i < pushArray.Length; i++) //Loops through the following code for the length of pushArray.
+            {
+                tempArray[i + 1] = pushArray[i]; //Sets the next value in tempArray to the current value in pushArray. 
+            }
+            return tempArray; //returns tempArray.
+        }
+        static string[] pop(string[] popArray) //Defines the pop method.
+        {
+            Console.WriteLine(popArray[0]); //Outputs the item at the beginning of the array.
+
+            string[] tempArray = new string[popArray.Length - 1]; //Creates a new temporary array with a length one lower than pushArray.
+
+            for (int i = 0; i < tempArray.Length; i++) //Loops through the following code for the length of tempArray.
+            {
+                tempArray[i] = popArray[i + 1]; //Sets the current value in tempArray to the next value in popArray
+            }
+            return tempArray; //Returns tempArray.
         }
 
-        static void peek(List<string> peekList) //Defines the peek method.
+        static void peek(string[] peekArray) //Defines the peek method.
         {
-            Console.WriteLine(peekList[0]); //Outputs the item at the beginning of the list.
-        }
-
-        static void pop(List<string> popList) //Defines the pop method.
-        {
-            Console.WriteLine(popList[0]); //Outputs the item at the beginning of the list.
-            popList.RemoveAt(0); //Removes the item at the beginning of the list
+            Console.WriteLine(peekArray[0]); //Outputs the item at the beginning of the array.
         }
     }
 }
